c7375c10365c92a7e62e54f411a523cd009f5067
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-06-10 13:54:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: detecting objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
