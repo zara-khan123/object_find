f55cebff553a228909c98093c0f9dd070288553c
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-06-11 09:15:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ function setup(){
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    f(status != "")
+    {objectDetector.detect(video, gotResults)};
+    for (i = 0; i < objects.length; i++ ) 
+    {
+        document.getElementById("status").innerHTML = "Status: object detected";
+        document.getElementById("number_of_objects").innerHTML = "number of objects detected are: "+objects.length;
+        fill('#ff0000');
+        percent = floor(objects[i].confidence * 100);
+        text = (objects[i].label + " " + percent + "%" ,objects[i].x + 15, objects[i].y + 15);
+        nofill();
+        stroke('#ff0000');
+        rect(object[i].x, object[i].y, objects[i].width, objects[i].height);
+
+    }
+}
 }
 
 function start()
@@ -29,3 +44,12 @@ function modelLoaded()
     video.speed(1);
     video.volume(0);
 }
+
+function gotResults(error, results)
+{
+if (error){
+    console.log(error);
+} else {
+console.log(results);
+objects = results;
+}}
